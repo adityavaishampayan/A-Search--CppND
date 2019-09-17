1f1f8f1389ad2586fa60973e09b48f3b2d074129
adityavaishampayan <aditya1.vaishampayan@gmail.com>
2019-09-16 23:19:55 -0400

    added readBoard and PrintBoard functions

--- A* Cpp/Astar_search/main.cpp
+++ A* Cpp/Astar_search/main.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#include<iostream>
+#include <iostream>
 #include <vector>
 #include <array>
+#include <fstream>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::vector;
+using std::ifstream;
 
 /*
  * 
@@ -39,8 +43,45 @@ using namespace std;
 
 
 */
+
+
+void PrintBoard(const std::vector<std::vector<int>> board){
+    
+    for(unsigned int i=0;i<board.size();i++){
+        for(unsigned int j = 0;j<board[i].size();j++){
+            std::cout<<board[i][j];
+        }
+        std::cout<<"\n";
+    }
+}
+
+void ReadBoardFile(std::string filepath){
+    std::ifstream board_file;
+    board_file.open(filepath);
+    if(board_file){
+        std::cout<<"The file stream has been created! "<<"\n";
+        std::string line;
+        while (std::getline(board_file, line)){
+            std::cout<<line<<"\n";
+        }
+    }
+    else{std::cout<<"no file stream"<<"\n";}
+    }
+
+
 int main()
 {
-	cout<<"hello world\n"<<endl;
+   
+//    my_file.open("/media/aditya/INSTALL and EXTRA/CppND/Astar-Search-CppND/A* Cpp/files/boards.txt");
+ 
+    std::vector<std::vector<int>> board = {{{0, 1, 0, 0, 0, 0},
+                                  {0, 1, 0, 0, 0, 0},
+                                  {0, 1, 0, 0, 0, 0},
+                                  {0, 1, 0, 0, 0, 0},
+                                  {0, 0, 0, 0, 1, 0}}};
+
+    //PrintBoard(board);
+    ReadBoardFile("/media/aditya/INSTALL and EXTRA/CppND/Astar-Search-CppND/A* Cpp/files/boards2.txt");
+
 	return 0;
 }
